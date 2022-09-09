f02f88fce19c93e0ee97838541d937b69998799f
Mauro Pequenino <mauroernesto5@gmail.com>
2022-09-10 00:12:03 +0200

    updated array

--- src/data_strutures/search/binary_search/BinarySearch.java
+++ src/data_strutures/search/binary_search/BinarySearch.java
@@ -6,7 +6,7 @@ public class BinarySearch {
      * Nao percorre toda lista
      */
     public static void main(String[] args) {
-        int [] arr = {8,6,9,2,5,3,1,4};
+        int [] arr = {1,2,3,4,5,6,7,8};
         System.out.println("Position found: " + search(arr, 4));
     }
 
