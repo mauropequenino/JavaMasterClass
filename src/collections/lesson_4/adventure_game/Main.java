36f979afe99f20f6c7e97ef85b78e8cd95e54056
Mauro Pequenino <mauroernesto5@gmail.com>
2022-09-17 16:00:41 +0200

    maps

--- src/collections/lesson_4/adventure_game/Main.java
+++ src/collections/lesson_4/adventure_game/Main.java
@@ -1,6 +1,7 @@
 package collections.lesson_4.adventure_game;
 
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
 import java.util.Scanner;
 
@@ -17,14 +18,45 @@ public class Main {
         locations.put(4, new Location(4, "You are in a valley beside a stream"));
         locations.put(5, new Location(5, "You are in the forest"));
 
+        locations.get(1).addExist("W", 2);
+        locations.get(1).addExist("E", 3);
+        locations.get(1).addExist("S", 4);
+        locations.get(1).addExist("N", 5);
+        locations.get(1).addExist("Q", 2);
+
+        locations.get(2).addExist("N", 5);
+        locations.get(2).addExist("Q", 0);
+
+        locations.get(3).addExist("w", 2);
+        locations.get(3).addExist("Q", 0);
+
+        locations.get(4).addExist("N", 1);
+        locations.get(4).addExist("W", 2);
+        locations.get(4).addExist("Q", 0);
+
+        locations.get(5).addExist("W", 2);
+        locations.get(5).addExist("S", 1);
+        locations.get(5).addExist("Q", 0);
+
         int loc = 1;
         while(true) {
             System.out.println(locations.get(loc).getDescription());
             if(loc == 0) {
                 break;
             }
-            loc = sc.nextInt();
-            if(!locations.containsKey(loc)) {
+
+            Map<String, Integer> exits = locations.get(loc).getExists();
+            System.out.print("Available exists are ");
+            for(String exit: exits.keySet()) {
+                System.out.print(exit + ", ");
+            }
+            System.out.println();
+
+            String direction = sc.nextLine().toUpperCase();
+
+            if(exits.containsKey(direction)) {
+                loc = exits.get(direction);
+            } else  {
                 System.out.println("You cannot go in that direction");
             }
         }
